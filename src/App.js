17d77ceee4427b0b5adb94249997fc4327b11dbc
theoigen <oigenZ@icloud.com>
2019-05-29 01:08:47 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css'
 import Login from './components/Login/Login'
+import Home from './components/Home'
 
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        <Login />
-      </div>
-    )
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Switch>
+                        <Route path="/" exact component={Home} />
+                        <Route path="/login" component={Login} />
+                    </Switch>
+                </div>
+            </Router>
+        )
+    }
 }
+
+
+/**
+ * @TODO
+ * import { Link } from 'react-router-dom'
+ * <Link to="/{path}"
+ *  <li>blah-blah</li>
+ * </Link>
+ */
 
 export default App
